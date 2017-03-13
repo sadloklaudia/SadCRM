4490b565231ab7eb367a56d679b2180305f20e07
Daniel <wilkowski.kontakt@gmail.com>
2017-03-13 12:00:48 +0100

    Added destroy.php

--- destroy.php
+++ destroy.php
@@ -1,5 +1,14 @@
 <?php
-$directory = "SadCRM";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-array_map('unlink', glob("$directory/*.*"));
-rmdir($directory);
+$path = 'SadCRM';
+
+if (in_array(strtoupper(PHP_OS),[ 'WIN32', 'WINNT', 'WINDOWS'])) {
+    exec(sprintf("rd /s /q %s", escapeshellarg($path)));
+}
+else {
+    exec(sprintf("rm -rf %s", escapeshellarg($path)));
+}
+
+echo "$path removed";
