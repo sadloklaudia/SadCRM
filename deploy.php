5bca3d848fd8832f5ac032ad061032fc9ae827f3
Daniel <wilkowski.kontakt@gmail.com>
2017-03-13 12:01:05 +0100

    Removed deploy

--- deploy.php
+++
@@ -1,9 +0,0 @@
-<?php
-$zip = new ZipArchive();
-if ($zip->open('deploy.zip')) {
-    $zip->extractTo('/public_html/SadCrmYay/');
-    $zip->close();
-    echo 'Deploy successful';
-} else {
-    echo 'Couldn\' open deploy.zip';
-}
