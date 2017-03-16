97efe1d7c0d0a761744fbfa40ac1c9c707865397
Daniel <wilkowski.kontakt@gmail.com>
2017-03-16 15:33:08 +0100

    Added deploy.php to repo

---
+++ deploy.php
@@ -0,0 +1,48 @@
+<?php
+
+header("Content-Type: text/plain");
+
+function removeFolder($directoryName)
+{
+    if (!is_dir($directoryName)) {
+        throw new Exception("Error opening directory $directoryName");
+    }
+    $directory = opendir($directoryName);
+    while (false !== ($file = readdir($directory))) {
+        if (!in_array($file, ['.', '..'])) {
+            $full = $directoryName . '/' . $file;
+            if (is_dir($full)) {
+                removeFolder($full);
+            } else {
+                if (!is_writable($full)) {
+                    makeWritable($full);
+                }
+                unlink($full);
+            }
+        }
+    }
+    closedir($directory);
+    rmdir($directoryName);
+}
+
+function makeWritable($filename)
+{
+    if (!chmod($filename, 0777)) {
+        throw new Exception("Found read-only file. Failed to change permission");
+    }
+}
+
+function deployFor($name)
+{
+    echo "Removing folder $name...\n";
+    try {
+        removeFolder($name);
+        echo "Removed folder $name.\n";
+        mkdir($name);
+        echo "Created empty folder $name\n";
+    } catch (Exception $exception) {
+        echo "Could not remove $name! " . $exception->getMessage() . "\n";
+    }
+}
+
+deployFor("SadCRM");
