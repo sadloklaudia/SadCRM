51fbe7d04a68fd90f54131a3c49a5dff7ed6d841
Daniel <wilkowski.kontakt@gmail.com>
2017-03-24 17:45:17 +0100

    Cleaned config

--- config/prod/config.php
+++ config/prod/config.php
@@ -1,4 +1,7 @@
 <?php
+use Application\Model\FileLogger;
+use Ouzo\Db\Dialect\MySqlDialect;
+
 $config['db']['dbname'] = 'u866879676_sad';
 $config['db']['user'] = 'u866879676_root';
 $config['db']['pass'] = '76a5ba50d31';
@@ -7,9 +10,9 @@ $config['db']['host'] = 'mysql.hostinger.pl';
 $config['db']['port'] = '3306';
 
 $config['global']['prefix_system'] = '';
-$config['sql_dialect'] = '\\Ouzo\\Db\\Dialect\\MySqlDialect';
+$config['sql_dialect'] = MySqlDialect::class;
 $config['debug'] = true;
 
-$config['logger']['default']['class'] = 'Application\Model\FileLogger';
+$config['logger']['default']['class'] = FileLogger::class;
 
 return $config;
--- config/test/config.php
+++ config/test/config.php
@@ -1,4 +1,6 @@
 <?php
+use Ouzo\Db\Dialect\MySqlDialect;
+
 $config['db']['dbname'] = 'sadcrm_test';
 $config['db']['user'] = 'ouzo';
 $config['db']['pass'] = 'password';
@@ -7,6 +9,6 @@ $config['db']['host'] = '127.0.0.1';
 $config['db']['port'] = '3306';
 
 $config['global']['prefix_system'] = '/SadCRM';
-$config['sql_dialect'] = '\\Ouzo\\Db\\Dialect\\MySqlDialect';
+$config['sql_dialect'] = MySqlDialect::class;
 
 return $config;
